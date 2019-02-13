f6a3c3f6b5922bb31621bf2801e72691e3e81af9
Gordon Tyler Malan <43392248+tylermalan@users.noreply.github.com>
2019-02-13 11:01:28 -0800

    Create client.h
    Initial starting file for header for client class.

---
+++ client.h
@@ -0,0 +1,23 @@
+/*
+This class creates client objects that store information about a given personal training client.
+*/
+
+#pragma once
+
+#include <string>
+
+class client
+{
+public:
+	client();									// Default constructor
+	client(std::string);						// Constructor with only one string for a name and no age
+	client(std::string, std::string);			// Constructor for full name and no age
+	client(std::string, std::string, int);		// Constructor for full name and age
+	~client();
+
+private:
+	std::string firstName;						// Client's first name
+	std::string lastName;						// Client's last name
+	int age;									// Client's age
+	std::string startDate;						// Date that the client started training
+};
